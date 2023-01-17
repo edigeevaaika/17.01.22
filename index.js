679b1b3054941020deceda2ac734c521934c983e
Aida <edigeeva.aika@mail.ru>
2023-01-17 16:32:22 +0600

    added examples

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 console.log("hello world");
+//console.log("Helllo word");
+//console.log("Aida");
+
+let example = "marker";
+let example2 = "январь";
+console.log(example);
+//console.log(example_2);
+
+example = "pencil";
+
+//console.log(example);
+
+
+let myCity = "Каракол";
+let myFirstName = "Aida";//строка
+let myFavoriteNumber = 8.2;//число
+
+//console.log(myCity);
+console.log("my city is "+ myCity);
+
+console.log( "my name is "+ myFirstName);
+
+console.log("my favorite number "+myFavoriteNumber);
+
+//чему равняется площадь(area) прямоугольника с шириной 30(а) и длиной 50 (b) см.
+let a = 150;
+let b = 128;
+let area = a*b;
+console.log("Площадь равняется " + area);
+
+//клиент хочет обменять 10000 сом на доллары.
+//Сколько он получил долларов по курсу 87.5
+let kgs = 10000;
+let rate = 87.5;
+let usd = kgs / rate;
+console.log("клиент получит " +usd);
+
+
+// клиент хочет отправить посылку размером
+// высота 30см , длина 40 см 
+// Чему равняется обьем посылки?
+let height = 30;
+let width = 50;
+let length = 40;
+let volume = height*width*length;
+console.log("обьем посылки равняется " +volume);
+
